7f15ab4d600901fb13ad2545b6962f4749093eb2
zhaoxinrong <1473233993@qq.com>
2019-05-14 20:17:02 +0800

    修改了microblog_index的登录注册模块

--- Consensus/users/urls.py
+++ Consensus/users/urls.py
@@ -6,4 +6,5 @@ app_name = 'user'
 urlpatterns = [
     path('register/', views.register_index, name='user_register'),
     path('login/', views.login_index, name='user_login'),
+    path('logout/', views.logout_index, name='user_logout'),
 ]
